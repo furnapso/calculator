abc697692117c886b6ccdb31ba58ef18c926a9ef
furnapso <luke.boland95@gmail.com>
2020-12-13 19:48:26 +1100

    clear and updateDisplay functions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const calcScreen = document.querySelector("#screen");
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener('click', updateDisplay);
+});
+
+document.querySelector(".clear"),addEventListener('click', clear);
+
+const operators = '÷×-+';
+
+let output = 0;
+
 function operate(operator, num1, num2) {
     switch (operator) {
         case  "+":
@@ -9,4 +20,13 @@ function operate(operator, num1, num2) {
         case "*":
             return num1 * num2;
     }
+}
+
+function clear() {
+    calcScreen.textContent = "";
+}
+
+function updateDisplay() {
+    let oldContent = calcScreen.textContent;
+    calcScreen.textContent = oldContent + this.textContent;
 }
