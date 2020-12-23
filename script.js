5dde6544ec65c22cb263d9d58438bf1f48951efc
furnapso <luke.boland95@gmail.com>
2020-12-23 15:22:57 +1100

    Completed updateDisplay function

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const calcScreen = document.querySelector("#screen");
 document.querySelectorAll("button").forEach(button => {
+    if (button.classList.contains("clear")) {return}
     button.addEventListener('click', updateDisplay);
 });
 
-document.querySelector(".clear"),addEventListener('click', clear);
+document.querySelector(".clear").addEventListener('click', clearDisplay);
 
 const operators = '÷×-+';
+const numbers = '1234567890'
 
 let output = 0;
 
@@ -13,20 +15,42 @@ function operate(operator, num1, num2) {
     switch (operator) {
         case  "+":
             return num1 + num2;
-        case "/":
+        case "÷":
             return num1 / num2;
         case "-":
             return num1 - num2;
-        case "*":
+        case "×":
             return num1 * num2;
     }
 }
 
-function clear() {
+function clearDisplay() {
     calcScreen.textContent = "";
+    output = 0;
 }
 
 function updateDisplay() {
-    let oldContent = calcScreen.textContent;
-    calcScreen.textContent = oldContent + this.textContent;
+    let currentContent = calcScreen.textContent;
+    let newContent = this.textContent;
+
+    if (operators.includes(currentContent.slice(-1))) {
+        if (operators.includes(newContent.slice(-1))) {
+            return
+        }
+
+        else {
+            calcScreen.textContent = currentContent + " " + newContent;
+        }
+    }
+
+    else {
+        if (operators.includes(newContent.slice(-1))) {
+            calcScreen.textContent = currentContent + " " + newContent;
+        }
+        
+        else {
+            calcScreen.textContent = currentContent + newContent;
+        }
+    }
+
 }
